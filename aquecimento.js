d330ceab45a8597c1e5f1be4ddc3f371ab007338
Jonathan <jonathanffarias@hotmail.com>
2022-08-26 20:28:59 -0300

    aquecimento

---
+++ aquecimento.js
@@ -0,0 +1,12 @@
+    for(i = 0; i < 10; i++){
+        document.write(i);
+        console.log(i)
+    }
+
+
+    j = 0;
+    while(j < 10){
+        console.log('numero da linha while ' + j);
+        j++;
+    }
+
