96d22f8ce0a5a1784d0972ce3e504fcc4cb0303f
Jonathan <jonathanffarias@hotmail.com>
2022-08-29 19:33:01 -0300

    switch

--- aquecimento.js
+++ aquecimento.js
@@ -10,3 +10,32 @@
         j++;
     }
 
+    m = 0; 
+    do{
+        console.log('numero da linha DO ' + m);
+        m++;
+    } while(m < 10);
+
+    a = 5;
+    b = 10;
+    if(a > b){
+        console.log('A é maior que o B')
+    } else {
+        console.log('B é maior que o A')
+    }
+
+    switch(aquecimento){
+        case 1:
+            "Você selecionou numero 1"
+            break;
+        case 2: 
+            "Você selecionou numero 2"
+            break;
+        case 3:
+            "Você selecionou numero 3"
+            break;
+        default:
+            "Não existe"
+            break;
+        
+    }
