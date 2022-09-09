66958c05ae52a73aa0f1864add01e65fea99ef52
Jonathan <jonathanffarias@hotmail.com>
2022-09-09 19:59:02 -0300

    aula 09/09

--- aula-02-09/atividade/serviço.js
+++ aula-02-09/atividade/serviço.js
@@ -1,21 +1,16 @@
-let serviço = prompt("Qual serviço a ser realizado?");
-let prazo = prompt("Qual prazo para realizar o serviço (Em dias)?");
-
-
-function calcularServiço(serviço, prazo){
-
-    console.log('o serviço' + serviço + 'fica no valor de' + valor)
-
-    let valor ='';
-
-    if(prazo == '1'){
-        this.valor = 'R$ 150,00'
-    }else if(prazo == '2'){
-        this.valor = 'R$100,00'
-    }else {
-        this.valor = 'R$50,00'
+let servico = prompt("Qual serviço a ser realizado? \n0 - Troca de Oleo \n1 - Alinhamento \n2 - Balanceamento");
+let prazo = prompt("Qual prazo para realizar o serviço ? \n1 - Um dia \n2 - Dois dias \n3 - Tres Dias");
+let tabela = [  ['R$ 200,00', 'R$ 150,00', 'R$ 100,00'],
+                ['R$ 350,00', 'R$ 200,00', 'R$ 150,00'],
+                ['--', '--', 'R$ 150,00']]
+                
+    function validaEntrada(dadoUsuario){
+        return dadoUsuario >= 0 && dadoUsuario <= 2 ? true : false;
     }
 
-    return valor;
-}
+    if(validaEntrada(servico) && validaEntrada(prazo)){
+        alert('o serviço ficara em: ' + tabela[servico][prazo])
+    } else {
+        alert('ERROR: Confira os valores inseridos')
+    }
 
