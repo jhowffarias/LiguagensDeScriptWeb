e305fad686be5e1a1077c95f2a36241edf86ec5c
Jonathan <jonathanffarias@hotmail.com>
2022-09-16 22:11:11 -0300

    Aula 16/09

--- aula-09-09/ex2.js
+++ aula-09-09/ex2.js
@@ -7,17 +7,18 @@ btnCalcular.addEventListener('click', function(event){
     //Obter os valores de serviço e prazo
     let Servico = document.getElementById('servico');
     let Prazo = document.getElementById('prazo');
+    let resultado = document.getElementById('resultado');
 
-    
+    let preco = calculaPreco(Servico.value, Prazo.value);
+
+    resultado.innerText = `O preço é ${preco}`;
+
+    function calculaPreco(Servico, Prazo){
+        let tabela = [  ['R$ 200,00', 'R$ 150,00', 'R$ 100,00'],
+                        ['R$ 350,00', 'R$ 200,00', 'R$ 150,00'],
+                        ['Não Fazemos', 'Não Fazemos', 'R$ 150,00']]
+
+        return tabela[Servico][Prazo];
+    }
 });
-
-
-// btnCalcular.addEventListener('click', executar());
-
-// function executar(event){
-//     console.log(event)
-
-//     //Obter os valores de serviço e prazol
-// }
-// console.log(btnCalcular);
 
